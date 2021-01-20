768d53f93e881f4c5dfc8d3bcf233fbe0a1bee1f
İpek Erdoğan <32425967+erdoganip@users.noreply.github.com>
2021-01-20 19:14:12 +0300

    Small name changes

--- feature_extraction/sift.py
+++ feature_extraction/sift.py
@@ -1,16 +1,26 @@
+###IPEK ERDOGAN COMPUTER VISION HW3 SIFT PART
+
 import os
 import cv2
-
+from scipy.spatial import distance
 import numpy as np
+from numpy.random import seed
+from numpy.random import randint
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import preprocessing
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
 
-def feature_extraction_sift(dirName,labels,feature_vectors,keypoints):
+#########IN THIS PART, I AM USING PREPROCESSING AND USING OPENCV SIFT IMPLEMENTATION TO EXTRACT THE DESCRIPTORS
+def descriptor_Extractor(dirName,labels,descriptors):
     listOfFile = os.listdir(dirName)
+    #listOfFile.sort()
     for entry in listOfFile:
         fullPath = os.path.join(dirName, entry)
-        if os.path.isdir(fullPath):
-            getListOfFiles(fullPath,labels,feature_vectors,keypoints)
+        if descriptor_Extractor(fullPath,labels,descriptors)
         elif(not (fullPath.startswith("Caltech20/training/.") or fullPath.startswith("Caltech20/testing/."))):
-            label = dirName
+            label = dirName.split("/")[2]
             img = cv2.imread(fullPath)
             #resizing images to decrease the computational cost
             img = cv2.resize(img,(150, 150))
@@ -21,6 +31,6 @@ def feature_extraction_sift(dirName,labels,feature_vectors,keypoints):
             #detect SIFT features in both images
             keypoint, descriptor = sift.detectAndCompute(img, None)
             if (len(keypoint) >= 1): #to get rid of None's coming from sift keypoints
-                feature_vectors.append(descriptor)
-                keypoints.append(keypoint)
+                descriptors.append(descriptor)
                 labels.append(label)
+##################################
